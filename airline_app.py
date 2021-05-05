6ec9365fa1f81d36087b54e1e794d799d62fef51
root <mryan6@zagmail.gonzaga.edu>
2021-05-05 03:05:58 +0000

    added user interface

--- airline_app.py
+++ airline_app.py
@@ -72,5 +72,5 @@ def predict_interviews_well(instance):
 
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 5000)
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
